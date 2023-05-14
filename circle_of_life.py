4ab29ac20101d1046aecbe2809d43fb94a28d1a4
ukwak <ukwakukwak1@gmail.com>
2023-05-14 22:44:42 +0900

    asdasd

--- circle_of_life.py
+++ circle_of_life.py
@@ -5,6 +5,7 @@ def print_TODO(todo):
     print(f'<<<NOT IMPLEMENTED: {todo}>>>')
 
 class CircleOfLife:
+
     def __init__(self, world_size, num_zebras, num_lions):
 
         #self.zebras = [Animal(0,0) for _ in range(num_zebras)]
@@ -29,17 +30,22 @@ class CircleOfLife:
         
         #para el 1234 de arriba
         coordenates = [f'{coord+1}' for coord in range(len(self.grid))]
-        print(((int(cell_size/2))*' ') + (cell_size*' ').join(coordenates))
+        print('   ' + ((int(cell_size/2))*' ') + (cell_size*' ').join(coordenates))
         # por la cantidad de cuadros por - te da cierta cantidad pero es insuficiente. Lo llenas con '--'
-        print("-" * ((cell_size + 1) * world_size - 1) + '--')
-        for row in self.grid:
-            print("|" + "|".join(row) + "|")
-            print("-" * ((cell_size + 1) * world_size - 1) + '--')
-       
+        print('   ' + "-" * ((cell_size + 1) * world_size - 1) + '--')
+        for row_idx, row in enumerate(self.grid):
+            print(f"{coordenates[row_idx]}  |" + "|".join(row) + "|")
+            print('   ' + "-" * ((cell_size + 1) * world_size - 1) + '--')
+
+            
         #print(grid)
 
+
+
     def display(self):
+
         print(f'time step: {self.timestep}')
+
         for animal in self.zebras:
             self.grid[animal.y][animal.x] = 'Z'
         for animal in self.lions:
